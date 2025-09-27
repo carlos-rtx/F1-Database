1c842b0fb5707f8674ede43b04b65df1a3fc9ea2
Charles-0000000 <carlossebasev@gmail.com>
2025-09-27 14:05:31 -0600

    this fucking stupid update

--- main.js
+++ main.js
@@ -2,7 +2,33 @@ window.addEventListener("DOMContentLoaded", (e) => {
   console.log("dom loaded successfully")
 });
 const menuButton = document.querySelector(".open-button");
+const langButton = document.querySelector(".lang-button");
+const langTxt = document.querySelector(".nav-lang-text")
 const navList = document.querySelector(".nav-list")
 menuButton.addEventListener("click", (e) => {
   navList.classList.toggle('novisible'); }
 )
+
+
+langTxt.addEventListener("click", (e) => {
+  if (langTxt.innerText === "EN") {
+  langTxt.innerText = "ES"
+  } else {
+    langTxt.innerText = "EN"
+  }
+})
+
+langButton.addEventListener("click", (e) => {
+  if (langTxt.innerText === "EN") {
+  langTxt.innerText = "ES"
+  } else {
+    langTxt.innerText = "EN"
+  }
+})
+
+/*langSel.forEach(function() {
+  elem.addEventListener("click", (e) => {
+    console.log("test");
+  })
+})*/
+//useless
