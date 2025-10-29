5af296ccb42ef5fe7f13edbc0d8b3d676fd68476
Charles-0000000 <skldld>
2025-10-29 12:11:09 -0600

    djjd

--- frontend /main.jsx
+++ frontend /main.jsx
@@ -1,7 +1,14 @@
-import React,  { useState } from "react";
-export default function F1DbMain() {
-  return 
-  <>
-//code here 
-  </>
-}
+// src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./main.css"; // global styles
+import "./fonts.css"; // font imports
+import ErrorModule from "/../../modules/error.js"
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
