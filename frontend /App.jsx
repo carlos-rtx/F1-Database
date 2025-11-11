7260303b1e6dace1f54068654c1d72c2abdf292f
Charles-0000000 <r>
2025-11-11 12:52:46 -0600

    update

--- frontend /App.jsx
+++ frontend /App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 //main document structure
 export default function F1DbMain() {
-  return {
-  }
+  return (
+    <>
+      //TODO: Refactor the whole project 
+    </>
+  )
 }
