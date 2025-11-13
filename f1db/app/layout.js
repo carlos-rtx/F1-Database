498e417e7c1494ec5185b0d128ac28d2bcdd2e15
Chärles <carlossebasev@gmail.com>
2025-11-13 07:01:57 +0000

    kojmewfijofmekrheijn op´fjj ij er r kjfjbrnb   ororororro m miniugmnmnmnmnmnmnmnmnm

--- f1db/app/layout.js
+++ f1db/app/layout.js
@@ -1,17 +1,54 @@
 import "../styles/globals.css";
-import "../styles/fonts.css"
+import { localFont } from 'next/font/local';
+import { Major_Mono_Display, Space_Grotesk } from 'next/font/google';
+
 import Header from "../components/Head_main.jsx"
-import NavBar from "../components/NavBar.jsx"; 
-// export const metadata = {
-//   title: "F1 Database",
-//   description: "f1db ",
-// };
+import NavBar from "../components/NavBar.jsx";
+
+const SFPro = localFont({
+  src: [
+    {
+      path: '../public/fonts/SFProDisplay-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+      format: 'woff2',
+    },
+    {
+      path: '../public/fonts/SFProDisplay-UltraLightItalic.woff2',
+      weight: '200',
+      style: 'normal',
+      format: 'woff2',
+    },
+    {
+      path: '../public/fonts/SFProDisplayMedium.woff2',
+      weight: '600',
+      style: 'normal',
+      format: 'woff2',
+    },
+    {
+      path: '../public/fonts/SFProDisplay-Bold.woff2',
+      weight: '800',
+      style: 'normal',
+      format: 'woff2',
+    },
+  ],
+});
+
+const majorMonoDisplay = Major_Mono_Display({
+  weight: '400',
+  subsets: ['latin'],
+});
+
+const spaceGrotesk = Space_Grotesk({
+  weight: ['300', '400', '500', '600', '700'],
+  subsets: ['latin'],
+});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <Header/>
+    <html lang="en" className={`${SFPro.className} ${majorMonoDisplay.className} ${spaceGrotesk.className}`}>
       <body>
+        <Header/>
         <NavBar/>
         {children}
       </body>
--- f1db/components/NavBar.jsx
+++ f1db/components/NavBar.jsx
@@ -18,7 +18,7 @@ setInterval(() => {console.log(window.innerWidth)}, 5000)
           <div className="nav-wrapper">
             <div className="nav-logo-container">
               <img
-                src="main-assets/imgbin_b5547cde756547246735f67decd7d946.png"
+                src="../images/imgbin_b5547cde756547246735f67decd7d946.png"
                 alt="logo"
               />
             </div>
@@ -27,13 +27,13 @@ setInterval(() => {console.log(window.innerWidth)}, 5000)
               <div className="nav-lang lang-selector">
                 <span className="nav-lang-text lang-selector">EN</span>
                 <img
-                  src="main-assets/language_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg"
+                  src="../images/language_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg"
                   alt="lang icon"
                   className="nav-button lang-button"
                 />
               </div>
               <img
-                src="main-assets/apps_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg"
+                src="../images/apps_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg"
                 alt="menu icon"
                 className="nav-button open-button"
               />
